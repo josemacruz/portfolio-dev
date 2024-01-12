b54f9f621f5bbf10419526ca64495f07b7f6035b
Josema Cruz <josemacruz9726@gmail.com>
2024-01-12 01:28:25 +0100

    Update Astro configuration and layout files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
   output: 'server',
   adapter: vercel(),
 });
---
+++ src/i18n/index.ts
@@ -0,0 +1,17 @@
+import spanish from './es.json'
+import english from './en.json'
+
+const LANGUAGES = {
+  SPANISH: 'es',
+  ENGLISH: 'en',
+}
+
+export const getI18N = ({
+  currentLocale = 'es'
+}: {
+  currentLocale: string | undefined
+}) => {
+  if (currentLocale === LANGUAGES.ENGLISH) return english
+  if (currentLocale === LANGUAGES.SPANISH) return spanish
+  return spanish
+}
