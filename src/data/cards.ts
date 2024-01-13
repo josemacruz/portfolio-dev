0c5314da5a0fce43da07016363c32a1063d08e85
Josema Cruz <josemacruz9726@gmail.com>
2024-01-13 19:58:41 +0100

    Update styles and components

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,22 +1,25 @@
 import LinkedIn from "../components/icons/LinkedIn.astro";
 import Github from "../components/icons/Github.astro";
-import ThemeSwitch from "../components/ThemeSwitch.astro";
 import About from "../components/About.astro";
 import Experience from "../components/Experience.astro";
 import Blog from "../components/Blog.astro";
+import Switch from "../components/Switch.astro";
 
 export const cards = [
   {
+    id: 'about',
     color: "#dfc4fe",
     children: About,
   },
   {
+    id: 'linkedin',
     color: "#0A66C2",
     children: LinkedIn,
     url: "https://www.linkedin.com/in/josemacruz/",
     urlType: "primary",
   },
   {
+    id:'experience_1',
     color: "var(--primary)",
     children: Experience,
     company: 'KNOWMAD_MOOD',
@@ -24,12 +27,14 @@ export const cards = [
     urlType: "secondary",
   },
   {
+    id: 'github',
     color: "var(--black)",
     children: Github,
     url: "https://github.com/josemacruz",
     urlType: "primary",
   },
   {
+    id: 'experience_2',
     color: "var(--primary)",
     children: Experience,
     company: 'SECMOTIC',
@@ -37,10 +42,12 @@ export const cards = [
     urlType: "secondary",
   },
   {
+    id: 'switch',
     color: "var(--primary)",
-    children: ThemeSwitch,
+    children: Switch,
   },
   {
+    id: 'blog',
     color: "var(--primary)",
     children: Blog,
     url: "https://ttcode.dev",
