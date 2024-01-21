1739978a761f239d6a8cc8f766384b23cb8f4e4d
Josema Cruz <josemacruz9726@gmail.com>
2024-01-21 22:59:21 +0100

    Add sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,6 @@ export default defineConfig({
   },
   output: 'server',
   adapter: vercel(),
+  site: "https://josemacruz.dev",
+  integrations: [sitemap()]
 });
